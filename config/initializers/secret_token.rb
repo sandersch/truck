75b61f2d9a7f5531d8a16c65ebffc8445c224045
Charlie Sanders <sanderscharlie@gmail.com>
2012-08-03 17:38:24 -0500

    Adds generated rails app
      > rails new truck

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Truck::Application.config.secret_token = 'd790daf0b27f50aa636971d7f7c72ee95cc8b095e5eb17072d4daaa13206df66c6efd39f695c4d0dc89d40d0de1d145e48349ed3c411fb346c579acbe41b75e6'
